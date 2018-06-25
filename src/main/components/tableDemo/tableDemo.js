64d2753c830f036a0308f48d0dc56b129e94643c
EJ Mercado <ej@ejmercado.com>
2018-06-26 01:33:04 +0800

    filter test

--- src/main/components/tableDemo/tableDemo.js
+++ src/main/components/tableDemo/tableDemo.js
@@ -3,9 +3,13 @@ import './tableDemo.css';
 function ComponentController(rawRunnerService) {
     const vm = this;
 
-    vm.sqlQuery = 'SELECT * FROM mwsdb.sales_last_12_months';
+    vm.sqlQuery = 'SELECT * FROM mwsdb._get_merchant_listings_all_data_';
     vm.tableData = [];
     vm.tableHeaders = [];
+    vm.tableHeaderFilters = {};
+    vm.objectFilter = null;
+
+    vm.change = () => console.log(vm.objectFilter);
 
     vm.loadData = () => {
         rawRunnerService.rawQuerySelect(vm.sqlQuery).then((data) => {
@@ -13,8 +17,32 @@ function ComponentController(rawRunnerService) {
             vm.tableData = tableData;
             vm.tableHeaders = data.data[1].map(value => value.name);
 
+            vm.createSelectData(vm.tableData, vm.tableHeaders);
+
             console.log(vm.tableData, vm.tableHeaders);
         });
+    };
+
+    vm.createSelectData = (tableData, tableHeaders) => {
+        for (let i = 0; i < tableHeaders.length; i++) {
+
+            const currentTableHeader = tableHeaders[i];
+            const seen = {};
+            const filterArray = [];
+            
+            for (let j = 0; j < tableData.length; j++) {
+                const currentTableData = tableData[j];
+                const currentTableDataValue = currentTableData[currentTableHeader];
+                if (!seen[currentTableDataValue]) {
+                    filterArray.push(currentTableDataValue);
+                    seen[currentTableDataValue] = true;
+                }
+            }
+
+            vm.tableHeaderFilters[currentTableHeader] = filterArray;
+        }
+
+        console.log(vm.tableHeaderFilters);
     };
 
     const activate = function activate() {
@@ -32,3 +60,4 @@ export default {
     controllerAs: '$ctrl',
     controller: ComponentController,
 };
+
