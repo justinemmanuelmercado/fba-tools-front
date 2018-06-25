9e4ee11c9806766eec15aa837039ce01eb1967de
carlcarambas <carlmark.carambas@gmail.com>
2018-06-26 00:50:39 +0800

    table sort and search initial

--- src/main/components/tableDemo/tableDemo.js
+++ src/main/components/tableDemo/tableDemo.js
@@ -1,31 +1,49 @@
 import './tableDemo.css';
 
-function ComponentController(rawRunnerService) {
+function ComponentController(rawRunnerService, orderBy) {
     const vm = this;
 
-    vm.sqlQuery = 'SELECT * FROM mwsdb.sales_last_12_months';
+    vm.sqlQuery = 'SELECT * FROM mwsdb.sales_last_12_months LIMIT 100';
     vm.tableData = [];
+    vm.fetchedData = [];
     vm.tableHeaders = [];
+
+    vm.propertyName = '';
+    vm.reverse = true;
+    vm.searchFilter = '';
 
     vm.loadData = () => {
         rawRunnerService.rawQuerySelect(vm.sqlQuery).then((data) => {
             const [tableData] = data.data;
+
+            vm.fetchedData = tableData;
             vm.tableData = tableData;
             vm.tableHeaders = data.data[1].map(value => value.name);
 
-            console.log(vm.tableData, vm.tableHeaders);
+            vm.propertyName = vm.tableHeaders[0];
+
         });
     };
 
     const activate = function activate() {
         vm.loadData();
+        vm.tableData = orderBy(vm.fetchedData, vm.propertyName, vm.reverse);
     };
+
+    vm.sortBy = (propertyName) => {
+        
+        vm.reverse = (vm.propertyName === propertyName) ? !vm.reverse : false;
+        vm.propertyName = propertyName;
+        vm.tableData = orderBy(vm.fetchedData, vm.propertyName, vm.reverse);
+        console.log(vm.propertyName, vm.reverse);
+        
+      };
 
     vm.$onInit = activate;
 
 }
 
-ComponentController.$inject = ['rawRunnerService'];
+ComponentController.$inject = ['rawRunnerService', 'orderByFilter'];
 
 export default {
     template: require('./tableDemo.html'),
