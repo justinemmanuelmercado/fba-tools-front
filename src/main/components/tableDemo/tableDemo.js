ef10f2d6e722831651becab5e65aaa26a33cf902
EJ Mercado <ej@ejmercado.com>
2018-06-26 12:39:46 +0800

    table-demo

--- src/main/components/tableDemo/tableDemo.js
+++ src/main/components/tableDemo/tableDemo.js
@@ -1,63 +1,64 @@
 import './tableDemo.css';
 
 function ComponentController(rawRunnerService) {
-    const vm = this;
+  const vm = this;
 
-    vm.sqlQuery = 'SELECT * FROM mwsdb._get_merchant_listings_all_data_';
-    vm.tableData = [];
-    vm.tableHeaders = [];
-    vm.tableHeaderFilters = {};
-    vm.objectFilter = null;
+  vm.sqlQuery = 'SELECT * FROM mwsdb._get_merchant_listings_all_data_';
+  vm.tableData = [];
+  vm.tableHeaders = [];
+  vm.tableHeaderFilters = {};
+  vm.objectFilter = {};
 
-    vm.change = () => console.log(vm.objectFilter);
+  vm.change = () => console.log(vm.objectFilter);
 
-    vm.loadData = () => {
-        rawRunnerService.rawQuerySelect(vm.sqlQuery).then((data) => {
-            const [tableData] = data.data;
-            vm.tableData = tableData;
-            vm.tableHeaders = data.data[1].map(value => value.name);
+  vm.loadData = () => {
+    rawRunnerService.rawQuerySelect(vm.sqlQuery).then((data) => {
+      const [tableData] = data.data;
+      vm.tableData = tableData;
+      vm.tableHeaders = data.data[1].map(value => value.name);
 
-            vm.createSelectData(vm.tableData, vm.tableHeaders);
+      vm.createSelectData(vm.tableData, vm.tableHeaders);
+      vm.createBlankObjectFilter(vm.tableHeaders);
+    });
+  };
 
-            console.log(vm.tableData, vm.tableHeaders);
-        });
-    };
+  vm.createBlankObjectFilter = (tableHeaders) => {
+    tableHeaders.forEach((header) => {
+      vm.objectFilter[header] = '!!';
+    });
+  };
 
-    vm.createSelectData = (tableData, tableHeaders) => {
-        for (let i = 0; i < tableHeaders.length; i++) {
+  vm.createSelectData = (tableData, tableHeaders) => {
+    for (let i = 0; i < tableHeaders.length; i++) {
 
-            const currentTableHeader = tableHeaders[i];
-            const seen = {};
-            const filterArray = [];
-            
-            for (let j = 0; j < tableData.length; j++) {
-                const currentTableData = tableData[j];
-                const currentTableDataValue = currentTableData[currentTableHeader];
-                if (!seen[currentTableDataValue]) {
-                    filterArray.push(currentTableDataValue);
-                    seen[currentTableDataValue] = true;
-                }
-            }
+      const currentTableHeader = tableHeaders[i];
+      const seen = {};
+      const filterArray = [];
 
-            vm.tableHeaderFilters[currentTableHeader] = filterArray;
+      for (let j = 0; j < tableData.length; j++) {
+        const currentTableData = tableData[j];
+        const currentTableDataValue = currentTableData[currentTableHeader];
+        if (!seen[currentTableDataValue]) {
+          filterArray.push(currentTableDataValue);
+          seen[currentTableDataValue] = true;
         }
+      }
 
-        console.log(vm.tableHeaderFilters);
-    };
+      vm.tableHeaderFilters[currentTableHeader] = filterArray;
+    }
+  };
 
-    const activate = function activate() {
-        vm.loadData();
-    };
-
-    vm.$onInit = activate;
+  vm.$onInit = function activate() {
+    vm.loadData();
+  };
 
 }
 
 ComponentController.$inject = ['rawRunnerService'];
 
 export default {
-    template: require('./tableDemo.html'),
-    controllerAs: '$ctrl',
-    controller: ComponentController,
+  template: require('./tableDemo.html'),
+  controllerAs: '$ctrl',
+  controller: ComponentController,
 };
 
